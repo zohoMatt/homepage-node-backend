e4bd75278cb4ddc6aef18e43c0ccd58cd4b3a29d
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2017-08-14 13:57:28 +0800

    feat: Modifying data before sending (project apis). || struct: Nodemon to watch all files

--- model/projectDataHandler.js
+++ model/projectDataHandler.js
@@ -7,19 +7,29 @@ const ccolor = require('colors');
 const Project = require('./Schema').Project;
 
 /**
- * Get all projects and their details.
+ * Get all projects and their details. Modify data and delete unnecessary attributes.
  *
  * @returns {Promise} Returns a Promise function passing the data through.
  */
 const getProjectsAsync = () => {
     return new Promise((resolve, reject) => {
-        Project.find({}, function (err, res) {
+        Project.find({}).lean().exec(function (err, res) {
             // Handling error
             if (err) {
                 console.log(`!! ERROR WHILE GET ALL PROJECTS !! ${err}`.red);
                 reject(err);
                 return;
             }
+            // Modifying result, removing unnecessary attributes
+            res.map((obj) => {
+                delete obj._id;
+                delete obj.__v;
+                obj.tagList.map((tag) => {
+                    delete tag._id;
+                    return tag;
+                });
+                return obj;
+            });
             // Passing params while succeeding
             resolve(res);
         });
